1025dfd6b5f2ff6b77893d6ac50e5ad7f03d70ca
Sahil Sinha <ssinha28@umd.edu>
2022-03-07 20:57:43 -0500

    Added SwiftUI Files

---
+++ Fooodify/Fooodify/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fooodify
+//
+//  Created by Group 5 on 3/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fooodify/Fooodify/FooodifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FooodifyApp.swift
+//  Fooodify
+//
+//  Created by Group 5 on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct FooodifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
